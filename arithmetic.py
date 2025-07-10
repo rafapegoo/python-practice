ff581fde3fe6b8b9dbfa1d6fbfc4323e86116e80
Rafaela <rafaelapegobela@gmail.com>
2025-07-09 21:52:52 -0300

    Adicionei +1 exerc. HackerRank

---
+++ arithmetic.py
@@ -0,0 +1,12 @@
+#Soma, Diferença e Produto
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    add = a + b
+    print(add)
+    sub = a - b 
+    print(sub)
+    mult = a * b
+    print(mult)
+    
